51c2a4effd1b205e709a983a9b357afdf4ca85c4
imaldi <aldiirsanmajid@gmail.com>
2023-06-28 15:10:02 +0700

    done adding forum section in landing page

--- lib/presentation/pages/landing_page.dart
+++ lib/presentation/pages/landing_page.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neraca_ruang/core/consts/colors.dart';
 import 'package:flutter_neraca_ruang/core/consts/num_consts.dart';
 import 'package:flutter_neraca_ruang/core/router/app_router.dart';
+import 'package:flutter_neraca_ruang/logic/state_management/riverpod/active_forum_providers.dart';
 import 'package:flutter_neraca_ruang/logic/state_management/riverpod/dashboard_providers.dart';
 import 'package:flutter_neraca_ruang/presentation/widgets/IconWidget.dart';
 import 'package:flutter_neraca_ruang/presentation/widgets/appbar_widget.dart';
@@ -41,6 +43,7 @@ class LandingPageState extends ConsumerState<LandingPage> {
     var adsense = ref.watch(adsenseProvider);
     var authData = ref.watch(authStatusProvider);
     var isLogin = authData.value != null;
+    var forumList = ref.watch(activeForumsProvider);
 
     return DefaultTabController(
       length: mainTabLength,
@@ -112,13 +115,13 @@ class LandingPageState extends ConsumerState<LandingPage> {
                                         child: Image.asset(iconKabar)),
                                     Text(
                                       // TODO fix formatting tanggal
-                                      '${data.first.sourceName ?? ""} 26/05/2023, 12:00 WIB',
+                                      '${data.first.sourceName ?? ""}, ${data.first.sourceDate ?? ""}',
                                       style:
                                           Theme.of(context).textTheme.bodySmall,
                                     ),
                                     Container(
-                                      margin: const EdgeInsets.symmetric(
-                                          vertical: medium),
+                                      margin:
+                                          const EdgeInsets.only(bottom: medium),
                                       child: Text(
                                         '${data.first.judul ?? "Kabar"}',
                                         style: Theme.of(context)
@@ -187,14 +190,14 @@ class LandingPageState extends ConsumerState<LandingPage> {
                                   IntrinsicHeight(
                                     child: Text(
                                       // TODO fix formatting tanggal
-                                      '${data[1].sourceName ?? ""} 26/05/2023, 12:00 WIB',
+                                      '${data[1].sourceName ?? ""}, ${data[1].sourceDate ?? ""}',
                                       style:
                                           Theme.of(context).textTheme.bodySmall,
                                     ),
                                   ),
                                   Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: medium),
+                                    margin:
+                                        const EdgeInsets.only(bottom: medium),
                                     child: Text(
                                       "${data[1].judul ?? "Jurnal"}",
                                       style: Theme.of(context)
@@ -297,9 +300,17 @@ class LandingPageState extends ConsumerState<LandingPage> {
                                             maxWidth: extra, maxHeight: extra),
                                         child: Image.asset(iconInfografis)),
                                   ),
+                                  IntrinsicHeight(
+                                    child: Text(
+                                      // TODO fix formatting tanggal
+                                      '${data[2].sourceName ?? ""}, ${data[2].sourceDate ?? ""}',
+                                      style:
+                                          Theme.of(context).textTheme.bodySmall,
+                                    ),
+                                  ),
                                   Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: medium),
+                                    margin:
+                                        const EdgeInsets.only(bottom: medium),
                                     child: Text(
                                       "${data[2].judul ?? "Infografis"}",
                                       style: Theme.of(context)
@@ -375,13 +386,13 @@ class LandingPageState extends ConsumerState<LandingPage> {
                                   ),
                                   Text(
                                     // TODO fix formatting tanggal
-                                    '${data[3].sourceName ?? ""} 26/05/2023, 12:00 WIB',
+                                    '${data[3].sourceName ?? ""}, ${data[3].sourceDate ?? ""}',
                                     style:
                                         Theme.of(context).textTheme.bodySmall,
                                   ),
                                   Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: medium),
+                                    margin:
+                                        const EdgeInsets.only(bottom: medium),
                                     child: Text(
                                       "${data[3].judul ?? "Video"}",
                                       style: Theme.of(context)
@@ -403,13 +414,30 @@ class LandingPageState extends ConsumerState<LandingPage> {
                                 maxHeight: 200,
                                 minWidth: MediaQuery.of(context).size.width,
                               ),
-                              child: Image.network(
-                                "https://$contentUrl/${data[4].images}" ?? "",
-                                fit: BoxFit.cover,
-                                errorBuilder: (bc, o, st) {
-                                  return Text(data[4].images ?? "");
-                                },
-                              ),
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: data[2].listMedia?.length ?? 0,
+                                  itemBuilder: (c, i) {
+                                    return SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        child:
+                                            data[4].listMedia?[i].images != null
+                                                ? FittedBox(
+                                                    fit: BoxFit.fitHeight,
+                                                    child: Image.network(
+                                                      data[4]
+                                                              .listMedia?[i]
+                                                              .images ??
+                                                          "",
+                                                      errorBuilder: (c, o, s) {
+                                                        return const IconWidget(
+                                                            iconError);
+                                                      },
+                                                    ),
+                                                  )
+                                                : const IconWidget(iconError));
+                                  }),
                             ),
                             Padding(
                               padding: const EdgeInsets.all(normal),
@@ -443,17 +471,13 @@ class LandingPageState extends ConsumerState<LandingPage> {
                                   ),
                                   Text(
                                     // TODO fix formatting tanggal
-                                    '${data[4].sourceName ?? ""} 26/05/2023, 12:00 WIB',
+                                    '${data[4].sourceName ?? ""}, ${data[4].sourceDate ?? ""}',
                                     style:
                                         Theme.of(context).textTheme.bodySmall,
                                   ),
-                                  IntrinsicHeight(
-                                    child: Text(
-                                        "${data[4].sourceName ?? ""}, ${data[4].sourceDate ?? ""}"),
-                                  ),
                                   Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: normal),
+                                    margin:
+                                        const EdgeInsets.only(bottom: normal),
                                     child: Text(
                                       "${data[4].judul ?? "Foto"}",
                                       style: Theme.of(context)
@@ -468,79 +492,78 @@ class LandingPageState extends ConsumerState<LandingPage> {
                             Divider(),
 
                             /// Diskusi section, hide dulu untuk sekarang
-                            // Padding(
-                            //   padding: const EdgeInsets.all(medium),
-                            //   child: Column(
-                            //     children: [
-                            //       Container(
-                            //           constraints: const BoxConstraints(
-                            //               maxWidth: extra, maxHeight: extra),
-                            //           child: Image.asset(iconForum)),
-                            //       Text(
-                            //         "APAKAH INDONESIA SUDAH MENJALANKAN OTONOMI DAERAH DENGAN BAIK? (STATIS, BELUM DARI API)",
-                            //         style:
-                            //             Theme.of(context).textTheme.headlineSmall,
-                            //         textAlign: TextAlign.center,
-                            //       ),
-                            //       const Text(
-                            //           "Diskusi hangat mengenai pandangan masyarakat terhadap penerapan otonomi daerah di masa kini. (STATIS, BELUM DARI API)"),
-                            //     ],
-                            //   ),
-                            // ),
-                            Container(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: extra),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    "IKUTI KAMI",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 8.0),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Image.asset(
-                                                "assets/images/icon_facebook.png"),
-                                            Image.asset(
-                                                "assets/images/icon_ig.png"),
-                                            Image.asset(
-                                                "assets/images/icon_twitter.png"),
-                                            Image.asset(
-                                                "assets/images/icon_tiktok.png"),
-                                            Image.asset(
-                                                "assets/images/icon_youtube.png"),
-                                          ],
-                                        ),
+                            forumList.when(
+                                data: (data) => Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: medium),
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            // margin: const EdgeInsets.only(bottom: normal),
+                                            constraints: BoxConstraints(
+                                              maxHeight: 200,
+                                              minWidth: MediaQuery.of(context)
+                                                  .size
+                                                  .width,
+                                            ),
+                                            child: Image.network(
+                                              "${data.first.threadThumbnail}",
+                                              fit: BoxFit.cover,
+                                              errorBuilder: (bc, o, st) {
+                                                return Center(
+                                                    child: Text(
+                                                        "Image Not Found"));
+                                              },
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: normal,
+                                          ),
+                                          InkWell(
+                                            onTap: () {
+                                              context.router.replace(
+                                                  const DiskusiRoute());
+                                            },
+                                            child: Container(
+                                                margin:
+                                                    const EdgeInsets.symmetric(
+                                                        vertical: normal),
+                                                constraints:
+                                                    const BoxConstraints(
+                                                        maxWidth: extra,
+                                                        maxHeight: extra),
+                                                child: Image.asset(iconForum)),
+                                          ),
+                                          SizedBox(
+                                            height: normal,
+                                          ),
+                                          Text(
+                                            // TODO fix formatting tanggal
+                                            '${data.first.createdAt ?? ""}',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall,
+                                          ),
+                                          Text(
+                                            data.first.threadSubject ?? "",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headlineSmall,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                          Text(data.first.threadDescription ??
+                                              ""),
+                                        ],
                                       ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 8.0),
-                                        child: Text(
-                                          "neracaruang@neracaruang.com",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodySmall
-                                              ?.copyWith(
-                                                  decoration:
-                                                      TextDecoration.underline),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
+                                    ),
+                                error: (o, st) =>
+                                    Center(child: Text("There is an error")),
+                                loading: () => Center(
+                                        child: CircularProgressIndicator(
+                                      color: Color(primaryColor),
+                                    ))),
 
+                            SocialMediaPanelWidget(),
                             // TODO implement adsense
                             Text(
                               "Ads",
